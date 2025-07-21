57b2c905bfaa013964844c6da04ee434d91ec331
seastj <dev.seastj@gmail.com>
2025-07-21 15:10:17 +0900

    [docs] Nive 차트 활용하기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Bar from "./components/chart/Bar";
+import Line from "./components/chart/Line";
 
 function App() {
   return (
@@ -7,6 +9,22 @@ function App() {
       <Routes>
         <Route path="/" element={<h1>홈</h1>}></Route>
         <Route path="/about" element={<h1>About</h1>}></Route>
+        <Route
+          path="/bar"
+          element={
+            <h1>
+              <Bar />
+            </h1>
+          }
+        ></Route>
+        <Route
+          path="/line"
+          element={
+            <h1>
+              <Line />
+            </h1>
+          }
+        ></Route>
       </Routes>
     </Router>
   );
